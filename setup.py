ee3b5e084e8280751cb51bb7f8ff3ce6c3977f83
greglee1905 <greglee1905@aol.com>
2020-07-30 15:39:11 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Identifying and recognizing organoids from confocal imagery.',
+    author='Greg Lee',
+    license='MIT',
+)
